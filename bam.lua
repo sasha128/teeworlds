1627ed4e50a9b0de9914bb22cc2d147adbc871c9
fisted <van.fisted@googlemail.com>
2010-12-17 21:36:32 +0000

    recreated repo, former was crapped up

--- bam.lua
+++ bam.lua
@@ -238,7 +238,7 @@ function build(settings)
 		engine, client, game_editor, zlib, pnglite, wavpack,
 		client_link_other, client_osxlaunch)
 
-	server_exe = Link(server_settings, "teeworlds_srv", engine, server,
+	server_exe = Link(server_settings, "tournament_srv", engine, server,
 		game_shared, game_server, zlib)
 
 	serverlaunch = {}
